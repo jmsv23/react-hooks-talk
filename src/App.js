307b4a48cdecfc4c13c577d6e5e81f469bdd4f3c
jmsv23 <jmsv23@gmail.com>
2019-10-07 15:13:24 -0500

    Basic structure

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Menu, Container } from 'semantic-ui-react';
+import 'semantic-ui-css/semantic.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function Home() {
+  return <h2>Home</h2>;
 }
 
-export default App;
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Menu>
+          <Menu.Item
+            name='home'
+          >
+            <Link to="/">Home</Link>
+        </Menu.Item>
+
+          <Menu.Item
+            name='about'
+          >
+            <Link to="/about">About</Link>
+        </Menu.Item>
+
+          <Menu.Item
+            name='users'
+          >
+            <Link to="/users">Users</Link>
+        </Menu.Item>
+        </Menu>
+        <Container>
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/users">
+              <Users />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </Container>
+      </Router>
+    );
+  }
+}
